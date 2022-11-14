cece2b2c549a7dcd7677e620a73954f766e0fcc7
Donald Iloekwe <donytex4u@gmail.com>
2022-11-14 09:55:29 +0000

    typescript-react-boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return <div>Routes Here</div>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import './styles/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);

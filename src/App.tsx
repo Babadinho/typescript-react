532ba4bb0aaf49fe0b61ec913a0c813c5d76942c
Donald Iloekwe <donytex4u@gmail.com>
2022-11-14 10:02:53 +0000

    typescript-rreact-boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  return <div>Routes Here</div>;
+  return <>Routes Here</>;
 };
 
 export default App;
